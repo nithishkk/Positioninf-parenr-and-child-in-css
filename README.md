a8c30c190476f10be9d67e7c0a469d46a4a8f3d6
nithishkk <109412506+nithishkk@users.noreply.github.com>
2023-05-05 17:52:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Positioninf-parenr-and-child-in-css
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1aptjc)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="parent">
+    <div className="child1">d</div>
+    <div className="child2">d</div>
+    <div className="child3">d</div>
+  
+  </div>
+  );
+}
